05627b906cbfb68924e08e884c03f38dcf70b4cc
Aman Shrivastava <amanshri.nitrr@gmail.com>
2025-04-26 21:07:58 +0530

    bst

---
+++ algorithms/trees/bst.py
@@ -0,0 +1,37 @@
+from typing import Optional
+
+
+class Node:
+    def __init__(self, val:int):
+        self.val= val
+        self.left: Optional["Node"] = None 
+        self.right: Optional["Node"] = None
+
+def insert(node: Optional["Node"], val):
+    if node is None:
+        return Node(val)
+    if val < node.val:
+        node.left = insert(node.left, val)
+    if val >= node.val:
+        node.right = insert(node.right, val)
+
+    return node
+
+
+def inorder(node:Optional["Node"]):
+    if node:
+        inorder(node.left)
+        print(node.val, end=' ')
+        inorder(node.right)
+
+
+r = Node(15)
+r = insert(r, 10)
+r = insert(r, 18)
+r = insert(r, 4)
+r = insert(r, 11)
+r = insert(r, 16)
+r = insert(r, 20)
+r = insert(r, 13)
+
+inorder(r)
